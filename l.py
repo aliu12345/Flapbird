07fc152dc115ca4bcbbd003f49bea5059c186c8f
Tony Liu <tony.chengliu2013@gmail.com>
2024-06-08 22:19:13 -0400

    added score function

--- l.py
+++ l.py
@@ -2,13 +2,18 @@ import time
 import random
 import pygame
 from pygame.locals import *
+
 pygame.mixer.init()
 pygame.init()
 screen = pygame.display.set_mode((800, 500), 0, 32)
 
+# Constants
 GRAVITY = 0.5
 JUMP_VELOCITY = -4
 BUILDING_SPEED = 3
+GAP_SIZE = 150  # Space between the buildings for the bird to fly through
+
+# Load assets
 jump_sound = pygame.mixer.Sound('./sounds/Jump.wav')
 score_color = (255, 255, 255)
 losing_color = (255, 0, 0)
@@ -21,38 +26,56 @@ bird = pygame.image.load('./images/Flappy-Bird-PNG-File.png')
 background = pygame.image.load("./images/bg.png")
 bird = pygame.transform.scale(bird, (100, 50))
 building = pygame.image.load('./images/building.png')
-
-building_copy = building.copy()
-# https://www.geeksforgeeks.org/pygame-flip-the-image/
 upside_down_building = pygame.image.load("./images/upsideDownBuilding.png")
 
 pygame.mixer.music.load('./sounds/BABABOOEY 2.wav')
 background = pygame.transform.scale(background, (800, 500))
 pygame.mixer.music.play(-1)
 vine_boom = pygame.mixer.Sound("./sounds/Vine boom sound effect slightly bass boosted.wav")
+lose_sound = pygame.mixer.Sound('./sounds/Oops.wav')
+
+
+# Game variables
 game_over = False
 x, y = (0, 200)
 y_velocity = 0
 building_x = 800
-lose_sound = pygame.mixer.Sound('./sounds/Oops.wav')
 upside_down_building_x = 800
 bird_passed_buildings = False
-size = (100, random.randint(0, 410))
 clock = pygame.time.Clock()
 losing_font = pygame.font.SysFont('Arial', 50)
 losing_text = losing_font.render('Game Over', True, losing_color)
-bird_rect = bird.get_rect(topleft=(x, y))
-building_rect = building.get_rect(topleft=(building_x, 300))
-upside_down_building_rect = upside_down_building.get_rect(topleft=(upside_down_building_x, 0))
-while not game_over:
 
-    building = pygame.transform.scale(building, size)
-    upside_down_building = pygame.transform.scale(upside_down_building, (100, 500 - size[1] - 80))
+
+# Function to generate new building sizes and positions
+def new_building_sizes():
+    bottom_building_height = random.randint(100, 300)
+    top_building_height = 500 - GAP_SIZE - bottom_building_height
+    return bottom_building_height, top_building_height
+
+
+bottom_building_height, top_building_height = new_building_sizes()
+
+# Initial rects
+bird_rect = bird.get_rect(topleft=(x, y))
+building_rect = building.get_rect(topleft=(building_x, 500 - bottom_building_height))
+upside_down_building_rect = upside_down_building.get_rect(topleft=(upside_down_building_x, 0))
+
+while not game_over:
+    # Scale buildings
+    building = pygame.transform.scale(building, (100, bottom_building_height))
+    upside_down_building = pygame.transform.scale(upside_down_building, (100, top_building_height))
+
+    # Render score text
+    print(score)
     score_text = score_font.render(f'Score: {score}', True, score_color)
+
+    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+    # Handle key presses
     pressed = pygame.key.get_pressed()
     if pressed[K_SPACE] or pressed[K_w] or pressed[K_UP]:
         y_velocity = JUMP_VELOCITY
@@ -65,19 +88,43 @@ while not game_over:
         y = 0
     if x < 0:
         x = 0
+    # Update bird position
+    y += y_velocity
+    y_velocity += GRAVITY
 
+    # Update building positions
+    building_x -= BUILDING_SPEED
+    building_create = False
 
-    # https://www.reddit.com/r/pygame/comments/14enzx4/how_to_use_recttopleft/
-    bird_rect.topleft = (x, y)
-    building_rect.topleft = (building_x, 300)
-    upside_down_building_rect.topleft = (upside_down_building_x, 0)
-    if building_rect[0] < bird_rect[0] and upside_down_building_rect[0] < bird_rect[0]:
-        add_score.play()
+    # Reset building positions if they move off-screen
+    if building_x < -100:  # Use a fixed width for the building
+        building_x = 800
+        bottom_building_height, top_building_height = new_building_sizes()
+        print("generate new building")
+        bird_passed_buildings = False
+        building_create = True
+
+    if upside_down_building_x < -100:  # Use a fixed width for the building
+        upside_down_building_x = 800
+        print("generate new upside-down building")
+        bird_passed_buildings = False
+        building_create = True
+
+    if building_create:
         score += 1
 
+    # Update rect positions
+    bird_rect.topleft = (x, y)
+    building_rect = building.get_rect(topleft=(building_x, 500 - bottom_building_height))
+    upside_down_building_rect = upside_down_building.get_rect(topleft=(upside_down_building_x, 0))
 
+    # Check if bird passed the buildings and update score
+    if building_rect.right < bird_rect.left and not bird_passed_buildings:
+        add_score.play()
+        score += 1
+        bird_passed_buildings = True  # Set the flag to prevent multiple score increments
 
-    # https://www.pygame.org/docs/ref/rect.html#pygame.Rect.colliderect
+    # Check for collisions
     if bird_rect.colliderect(building_rect) or bird_rect.colliderect(upside_down_building_rect):
         screen.fill((0, 0, 0))
         pygame.mixer.music.stop()
@@ -89,26 +136,14 @@ while not game_over:
         time.sleep(4)
         game_over = True
 
-    y += y_velocity
-    y_velocity += GRAVITY
-    upside_down_building_x -= BUILDING_SPEED
-    building_x -= BUILDING_SPEED
-    if building_x < -building.get_width():
-        building_x = 800
-        size = (100, random.randint(0, 410))  # Update the building size
-        bird_passed_buildings = False
-
-    if upside_down_building_x < -upside_down_building.get_width():
-        upside_down_building_x = 800
-        bird_passed_buildings = False
-
-
+    # Draw everything
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
     screen.blit(score_text, (0, 0))
-    screen.blit(bird, (x, y))
-    screen.blit(upside_down_building, (upside_down_building_x, 0))
-    screen.blit(building, (building_x, 300))
+    screen.blit(bird, bird_rect.topleft)
+    screen.blit(upside_down_building, (building_x, 0))
+    screen.blit(building, (building_x, 500 - bottom_building_height))
     pygame.display.update()
     clock.tick(60)
+
 pygame.quit()
